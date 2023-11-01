2c708ee281a562be6dc72066b08ae88075b4fd6c
马晓龙 <moxy.mxl@gmail.com>
2023-11-01 21:42:12 +0800

    新增启动aop代理

--- gloam-framework-core/src/main/java/com/gloamframework/core/boot/BootConfigure.java
+++ gloam-framework-core/src/main/java/com/gloamframework/core/boot/BootConfigure.java
@@ -3,8 +3,13 @@ package com.gloamframework.core.boot;
 import com.gloamframework.core.boot.context.SpringContext;
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+/**
+ * 启动aop代理
+ */
 @Configurable
+@EnableAspectJAutoProxy(exposeProxy=true, proxyTargetClass=true)
 public class BootConfigure {
 
     @Bean
