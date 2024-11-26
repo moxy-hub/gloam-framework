30d547411a0561b9061fa561d9ebcd1f2096a47d
马晓龙 <moxy.mxl@gmail.com>
2024-11-26 15:54:01 +0800

    [fix]:修复信封加密的密钥对过期时间单位不正确

--- gloam-framework-web/gloam-web-envelope/src/main/java/com/gloamframework/web/envelope/rsa/RsaService.java
+++ gloam-framework-web/gloam-web-envelope/src/main/java/com/gloamframework/web/envelope/rsa/RsaService.java
@@ -19,7 +19,7 @@ public class RsaService {
     /**
      * todo 默认的失效时间，目前版本可能存在，在失效临界值的时候出现，外部请求和内部校验使用两套密钥对的问题，需要在后续版本进行优化
      */
-    private static final long DEFAULT_RSA_EXPIRE_TTL = 24 * 60 * 60L;
+    private static final long DEFAULT_RSA_EXPIRE_TTL = 24 * 60 * 60 * 1000L;
     private static final String RSA_KEY = "RSA_KEY:%s";
 
     @Autowired
